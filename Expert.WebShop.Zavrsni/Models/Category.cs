3dd4b889fea5448f24a560193f897ae12205fbc8
pbaricic <paskobaricic.abc@gmail.com>
2023-05-19 13:38:15 +0200

    Login page - alpha

---
+++ Expert.WebShop.Zavrsni/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Expert.WebShop.Zavrsni.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool IsHidden { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
---
+++ Expert.WebShop.Zavrsni/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Expert.WebShop.Zavrsni.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Discount { get; set; }
+        public int CategoryId { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
