d224dd4307fc18cff89fd4a28e289638ee9daa48
pbaricic <paskobaricic.abc@gmail.com>
2023-05-20 19:43:11 +0200

    index

---
+++ Expert.WebShop.Zavrsni/Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace Expert.WebShop.Zavrsni.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
