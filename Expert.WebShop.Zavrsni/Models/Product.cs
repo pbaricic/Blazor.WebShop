9d6f67c0be714b58042b33c808ee9bcbdc2a546d
pbaricic <paskobaricic.abc@gmail.com>
2023-05-21 15:45:44 +0200

    AddProduct, index u grid riješen

--- Expert.WebShop.Zavrsni/Models/Product.cs
+++ Expert.WebShop.Zavrsni/Models/Product.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string ProductName { get; set; }
-        public string ProductDescription { get; set; }
+        public string? ProductDescription { get; set; }
         public decimal ProductPrice { get; set; }
         public int Discount { get; set; }
         public int CategoryId { get; set; }
