034cfe1aa01bf3c0b3eb153505efc21242eed896
pbaricic <paskobaricic.abc@gmail.com>
2023-05-23 14:08:17 +0200

    Dodavanje u košaricu riješeno

---
+++ Expert.WebShop.Zavrsni/Models/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿namespace Expert.WebShop.Zavrsni.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+        public int NumberOfItems { get; set; }
+        public string OrderedByName { get; set; }
+        public string OrderedByEmail { get; set; }
+        public string OrderId { get; set; }
+    }
+}
---
+++ Expert.WebShop.Zavrsni/Models/ShoppingCartInMemory.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+using System.Net.WebSockets;
+
+namespace Expert.WebShop.Zavrsni.Models
+{
+    public class ShoppingCartInMemory
+    {
+        private readonly HttpClient _httpClient;
+        public List<ShoppingCart> SelectedItems { get; set; } = new();
+
+        //Konstruktor klase ide sada 
+
+        public ShoppingCartInMemory(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddToShoppingCart(int productId)
+        {
+            if(SelectedItems.FirstOrDefault(x=>x.ProductId == productId) == null)
+            {
+                var result = await _httpClient.GetAsync($"https://expertshopapi.azurewebsites.net/api/Products/{productId}");
+                if (result.IsSuccessStatusCode)
+                {
+                    var addProduct = await result.Content.ReadFromJsonAsync<Product>();
+                    ShoppingCart addToList = new ShoppingCart();
+                    addToList.Product = addProduct;
+                    addToList.ProductId = addProduct.Id;
+                    addToList.NumberOfItems = 1;
+                    SelectedItems.Add(addToList);  
+                }
+            }
+
+        }
+    }
+}
