e8ce2c10f56e2c7b6326a1296684e2c300867853
pbaricic <paskobaricic.abc@gmail.com>
2023-05-28 19:54:00 +0200

    Dovršena administracija, ostalo alert za delete.

---
+++ Expert.WebShop.Zavrsni/Models/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Expert.WebShop.Zavrsni.Models
+{
+    public static class Constants
+    {
+        public static readonly string baseUrl = "https://expertshopapi.azurewebsites.net/api";
+        public static readonly string LoadError = "Imamo problema sa učitavanjem sadržaja! Molimo pokušajte ponovno!";
+    }
+}
