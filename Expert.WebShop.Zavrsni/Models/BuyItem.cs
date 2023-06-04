653755125ae1fdf8e1d10f4073554ddc4ac1f583
pbaricic <paskobaricic.abc@gmail.com>
2023-06-04 13:13:27 +0200

    zadnje

---
+++ Expert.WebShop.Zavrsni/Models/BuyItem.cs
@@ -0,0 +1,22 @@
+﻿namespace Expert.WebShop.Zavrsni.Models
+{
+    public class BuyItem
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string OrderedByName { get; set; } = null!;
+
+        public string OrderedByEmail { get; set; } = null!;
+
+        public Guid OrderId { get; set; }
+        public int NumberOfItems { get; set; }
+
+        public decimal PricePerProduct { get; set; }
+        public int ProductDiscount { get; set; }
+        public decimal TotalAmmount { get; set; }
+        public bool IsPayed { get; set; }
+        public bool IsDelivered { get; set; }
+    }
+}
